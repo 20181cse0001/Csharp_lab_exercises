6d591ea78cc7a0fd780d64f717108ae4a386ab7a
20181cse0001 <69751028+20181cse0001@users.noreply.github.com>
2020-09-25 03:58:34 +0530

    the varables program

---
+++ testvar/testvar/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testvar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte num1 ;
+            num1 = 5;
+
+            string strName;
+            strName = "Hello";
+
+            int intNum;
+            intNum = 144;
+
+            char c;
+            c = 'A';
+
+            bool b;
+            b = true;
+
+            double myNumber = 12.56;
+
+
+            Console.WriteLine(num1);
+            Console.WriteLine(strName);
+            Console.WriteLine(intNum);
+            Console.WriteLine(c);
+            Console.WriteLine(b);
+            Console.WriteLine(myNumber);
+
+
+
+
+        }
+    }
+}
