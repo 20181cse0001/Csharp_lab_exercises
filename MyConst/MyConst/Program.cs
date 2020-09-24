e3a3f64abb8172e75e52c3592649a0b4ed48e092
20181cse0001 <69751028+20181cse0001@users.noreply.github.com>
2020-09-25 04:19:43 +0530

    class of constant

---
+++ MyConst/MyConst/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int months = 12;
+            const int days = 365;
+            const int weeks = 52;
+            const double pi = 3.14;
+            const int speedoflight = 300000;
+
+            Console.WriteLine(months);
+            Console.WriteLine(days);
+            Console.WriteLine(weeks);
+            Console.WriteLine(pi);
+            Console.WriteLine(speedoflight);
+
+
+        }
+    }
+}
