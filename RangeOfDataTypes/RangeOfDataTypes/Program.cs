6182ee427c51a81f429f164712b43a65814dd72b
20181cse0001 <69751028+20181cse0001@users.noreply.github.com>
2020-09-25 05:37:02 +0530

    range

---
+++ RangeOfDataTypes/RangeOfDataTypes/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangeOfDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("byte");
+            Console.WriteLine(byte.MinValue);
+            Console.WriteLine(byte.MaxValue);
+            Console.WriteLine("===========");
+
+            Console.WriteLine("sbyte");
+            Console.WriteLine(sbyte.MinValue);
+            Console.WriteLine(sbyte.MaxValue);
+            Console.WriteLine("=============");
+
+            Console.WriteLine("long");
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine("==========");
+
+            Console.WriteLine("ulong");
+            Console.WriteLine(ulong.MinValue);
+            Console.WriteLine(ulong.MaxValue);
+            Console.WriteLine("===========");
+
+            Console.WriteLine("short");
+            Console.WriteLine(short.MinValue);
+            Console.WriteLine(short.MaxValue);
+            Console.WriteLine("===========");
+
+            Console.WriteLine("ushort");
+            Console.WriteLine(ushort.MinValue);
+            Console.WriteLine(ushort.MaxValue);
+            Console.WriteLine("===========");
+
+            Console.WriteLine("int");
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine("=========");
+
+            Console.WriteLine("uint");
+            Console.WriteLine(uint.MinValue);
+            Console.WriteLine(uint.MaxValue);
+            Console.WriteLine("==========");
+
+            Console.WriteLine("float");
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(float.MaxValue);
+            Console.WriteLine("==========");
+
+            Console.WriteLine("double");
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine("============");
+
+            Console.WriteLine("decimal");
+            Console.WriteLine(decimal.MinValue);
+            Console.WriteLine(decimal.MaxValue);
+            Console.WriteLine("============");
+
+
+
+
+              
+
+        }
+    }
+}
